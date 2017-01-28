667de9137968d8c5b5209836b227837b0876cd04
Alex Aralis <alex.aralis@gmail.com>
2017-01-28 18:16:08 -0500

    change es2015 to latest

--- dev.webpack.config.js
+++ dev.webpack.config.js
@@ -12,7 +12,7 @@ module.exports = {
         loader: 'babel-loader',
         exclude: /node_modules/,
         query: {
-          presets: ['es2015', 'stage-0', 'react']
+          presets: ['latest', 'stage-0', 'react']
         }
       }
     ]
