60d8ed8e39f5647a169babf4d9498bcd167a630c
Alex Aralis <alex.aralis@gmail.com>
2017-01-28 16:19:16 -0500

    babel, webpack etc.

---
+++ gulpfile.js
@@ -0,0 +1,7 @@
+import gulp from 'gulp';
+import babel from 'gulp-babel';
+import gulpWebpack from 'gulp-webpack';
+import path from 'path';
+import changed from 'gulp-changed';
+import del from 'del';
+import gutil from 'gulp-util';
