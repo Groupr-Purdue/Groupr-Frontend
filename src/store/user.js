50b4452ac142b6a32d49c4baca1c29bd2071b894
Alex Aralis <alex.aralis@gmail.com>
2017-03-25 16:05:17 -0400

    When registering users, send last name as both first and last name.

--- src/store/user.js
+++ src/store/user.js
@@ -73,6 +73,8 @@ export const registerUser = payload =>
       'password',
     ].forEach(mapOn(cleanPayload)(payload));
 
+    cleanPayload.first_name = cleanPayload.last_name;
+
     fetch(
       `${BACKEND_URL}/register`,
       {
